189f191978e7c9b75cff2c6ed78f48a41a469e0b
Priyank Kalgaonkar <pkalgaonkar2010@gmail.com>
2022-12-21 01:01:34 +0000

    Push from Keil
    Signed-off-by: Priyank Kalgaonkar <pkalgaonkar2010@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
 DigitalIn button(D6);
 
-int main()
-{
-    while(1){
-        if(button){
+int main() {
+    while(1) {
+        if(button) {
             printf("Button Pressed\n\r");
-            wait(0.7); // simple debouncing
+            wait_us(700000); // simple debouncing
         }           
     }
-
 }
